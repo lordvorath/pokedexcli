c166e6aaed0ea92e4d15bded56a111dd278bee96
Stefano Trentin <4kill3@gmail.com>
2025-05-01 11:52:24 +0200

    initial REPL implementation

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Print("Hello, World!")
+	reader := bufio.NewReader(os.Stdin)
+	input := bufio.NewScanner(reader)
+
+	for {
+		fmt.Print("Pokedex > ")
+		var text string
+		if input.Scan() {
+			text = input.Text()
+			words := cleanInput(text)
+			fmt.Printf("Your command was: %s\n", words[0])
+		} else {
+			return
+		}
+	}
 }
 
 func cleanInput(text string) []string {
